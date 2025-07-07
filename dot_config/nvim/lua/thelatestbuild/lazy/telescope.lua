95c659d17b8a62d58d1f1819b3e5f99b2414089c
Stephen Flee <steveflee@gmail.com>
2025-07-07 17:58:45 +0000

    Added

--- dot_config/nvim/lua/thelatestbuild/lazy/telescope.lua
+++ dot_config/nvim/lua/thelatestbuild/lazy/telescope.lua
@@ -6,6 +6,7 @@ return {
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
             vim.keymap.set('n', '<leader>ps', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
             end)
