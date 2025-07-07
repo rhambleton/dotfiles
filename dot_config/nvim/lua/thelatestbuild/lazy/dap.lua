ef433da04203c29d8da0d90ede7b73c736376059
Stephen Flee <steveflee@gmail.com>
2025-07-07 15:20:08 +0000

    Fixed DAP integration, turns out I was dumb

--- dot_config/nvim/lua/thelatestbuild/lazy/dap.lua
+++ dot_config/nvim/lua/thelatestbuild/lazy/dap.lua
@@ -4,12 +4,24 @@ return {
         dependencies = {
             "rcarriga/nvim-dap-ui",
             "nvim-neotest/nvim-nio",
+            "jay-babu/mason-nvim-dap.nvim",
+            "williamboman/mason.nvim",
             "leoluz/nvim-dap-go",
+            'theHamsta/nvim-dap-virtual-text',
         },
         config = function()
-            local dap, dapui = require("dap"), require("dapui")
+            require("mason-nvim-dap").setup({
+                ensure_installed = {
+                    "delve",
+                },
+                automatic_installation = true,
+            })
+
             require("dapui").setup()
             require("dap-go").setup()
+            require("nvim-dap-virtual-text").setup()
+
+            local dap, dapui = require("dap"), require("dapui")
             vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Continue" })
             vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
             vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
