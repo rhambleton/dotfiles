7f031995d27c5d6b8e9cac2c3739ae17dc1d0582
Stephen Flee <steveflee@gmail.com>
2025-07-08 22:12:19 +0000

    Adds tmux confix and fixed nested panes

--- dot_config/nvim/lua/thelatestbuild/lazy/colors.lua
+++ dot_config/nvim/lua/thelatestbuild/lazy/colors.lua
@@ -1,10 +1,13 @@
 return {
-	{
-		"catppuccin/nvim", 
-		name = "catppuccin", 
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme "catppuccin-mocha"
-		end
-	}
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true
+            })
+            vim.cmd.colorscheme "catppuccin-mocha"
+        end
+    }
 }
