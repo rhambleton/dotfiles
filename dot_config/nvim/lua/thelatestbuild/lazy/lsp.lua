b84dc2b5c5b32f523e9fa1fa64b33553a3aca7d6
Stephen Flee <steveflee@gmail.com>
2025-07-08 00:30:38 +0000

    Reverts treesitter

--- dot_config/nvim/lua/thelatestbuild/lazy/lsp.lua
+++ dot_config/nvim/lua/thelatestbuild/lazy/lsp.lua
@@ -8,7 +8,7 @@ return {
         config = function()
             require("mason").setup()
             require("mason-lspconfig").setup {
-                ensure_installed = { "lua_ls", "gopls", "nil_ls" },
+                ensure_installed = { "lua_ls", "gopls" },
             }
             vim.diagnostic.config({
                 virtual_text = true, -- Shows errors inline
