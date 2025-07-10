de10cb201484c72d6b432e5dfb8de022fd411538
Stephen Flee <steveflee@gmail.com>
2025-07-10 00:10:08 +0000

    changes color scheme to rose pine

--- dot_config/nvim/lua/thelatestbuild/lazy/colors.lua
+++ dot_config/nvim/lua/thelatestbuild/lazy/colors.lua
@@ -1,26 +1,39 @@
 return {
     {
-        "folke/tokyonight.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-            "nvim-lualine/lualine.nvim",
-        },
-        lazy = false,
-        priority = 1000,
-        opts = {},
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function()
-            require("tokyonight").setup({
-                -- style = "night"
-            })
-            require('lualine').setup {
-                options = {
-                    -- ... your lualine config
-                    theme = 'tokyonight-night'
-                    -- ... your lualine config
-                }
+            vim.cmd("colorscheme rose-pine")
+
+            local groups = {
+                "Normal", "NormalNC", "NormalFloat",
+                "FloatBorder", "WinSeparator", "VertSplit",
+                "StatusLine", "StatusLineNC", "EndOfBuffer",
+                "LineNr", "SignColumn", "CursorLineNr"
             }
-            vim.cmd.colorscheme "tokyonight-night"
+
+            for _, group in ipairs(groups) do
+                vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+            end
         end
+        -- "folke/tokyonight.nvim",
+        -- dependencies = {
+        --     "nvim-tree/nvim-web-devicons",
+        --     "nvim-lualine/lualine.nvim",
+        -- },
+        -- lazy = false,
+        -- priority = 1000,
+        -- opts = {},
+        -- config = function()
+        --     require('lualine').setup {
+        --         options = {
+        --             -- ... your lualine config
+        --             theme = 'tokyonight-night'
+        --             -- ... your lualine config
+        --         }
+        --     }
+        --     vim.cmd.colorscheme "tokyonight-night"
+        -- end
     }
 
 }
